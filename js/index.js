4bf0602d3df2d4b0c1b7fddb084cda183c74cd4e
Anthony Cortese <akcortese@Anthonys-MacBook-Pro.local>
2021-01-12 19:25:47 -0700

    completed steps

--- js/index.js
+++ js/index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+let navLink = document.querySelector('.nav-link');
+navLink.forEach(item => {
+    item.addEventListener('mouseover', () => {
+        item.style.color = 'yellow'
+    })
+})
+
